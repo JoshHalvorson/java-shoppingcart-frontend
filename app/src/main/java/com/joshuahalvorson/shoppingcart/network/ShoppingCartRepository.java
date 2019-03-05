b374edbbe1ef87e3854bae22d2de54eb18e7d980
Josh Halvorson <joshhalvorson16@gmail.com>
2019-03-05 13:17:15 -0800

    Add network repo and viewmodel

---
+++ app/src/main/java/com/joshuahalvorson/shoppingcart/network/ShoppingCartRepository.java
@@ -0,0 +1,37 @@
+package com.joshuahalvorson.shoppingcart.network;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.util.Log;
+
+import com.joshuahalvorson.shoppingcart.model.Product;
+
+import java.io.IOException;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ShoppingCartRepository {
+    private static final String TAG = "ShoppingCartRepository";
+
+    private static Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(ShoppingCartClient.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+    private static ShoppingCartClient client = retrofit.create(ShoppingCartClient.class);
+
+
+    public static List<Product> getAllProducts(){
+        Call<List<Product>> call = client.getAllProducts();
+        try {
+            Response<List<Product>> response = call.execute();
+            return response.body();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/com/joshuahalvorson/shoppingcart/network/ShoppingCartViewModel.java
@@ -0,0 +1,13 @@
+package com.joshuahalvorson.shoppingcart.network;
+
+import android.arch.lifecycle.ViewModel;
+
+import com.joshuahalvorson.shoppingcart.model.Product;
+
+import java.util.List;
+
+public class ShoppingCartViewModel extends ViewModel {
+    public List<Product> getAllProducts(){
+        return ShoppingCartRepository.getAllProducts();
+    }
+}
