a585e3dae5a5201c20d68f9ef39449f656ff542b
JoshHalvorson <joshhalvorson16@gmail.com>
2019-03-05 14:56:26 -0800

    Add get all products method

--- app/src/main/java/com/joshuahalvorson/shoppingcart/view/fragment/ShopFragment.java
+++ app/src/main/java/com/joshuahalvorson/shoppingcart/view/fragment/ShopFragment.java
@@ -1,17 +1,27 @@
 package com.joshuahalvorson.shoppingcart.view.fragment;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.joshuahalvorson.shoppingcart.R;
+import com.joshuahalvorson.shoppingcart.model.Product;
+import com.joshuahalvorson.shoppingcart.network.ShoppingCartViewModel;
+
+import java.util.List;
 
 public class ShopFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
+
+    private ShoppingCartViewModel viewModel;
 
     public ShopFragment() {
     }
@@ -31,6 +41,33 @@ public class ShopFragment extends Fragment {
         if (mListener != null) {
             mListener.onShopFragmentInteraction(uri);
         }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        viewModel = ViewModelProviders.of(this).get(ShoppingCartViewModel.class);
+        
+        getAllProducts();
+
+    }
+
+    private void getAllProducts() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                List<Product> products = viewModel.getAllProducts();
+                for(Product p : products){
+                    Log.i("product", p.getProductName());
+                }
+            }
+        }).start();
     }
 
     @Override
