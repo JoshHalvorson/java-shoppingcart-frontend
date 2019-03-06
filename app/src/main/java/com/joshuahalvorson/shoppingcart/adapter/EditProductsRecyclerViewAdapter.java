e7ccc71965b132e042e6a07f50e7da06284bab1e
Josh Halvorson <joshhalvorson16@gmail.com>
2019-03-06 15:44:44 -0800

    Add RecyclerView for editing products

---
+++ app/src/main/java/com/joshuahalvorson/shoppingcart/adapter/EditProductsRecyclerViewAdapter.java
@@ -0,0 +1,98 @@
+package com.joshuahalvorson.shoppingcart.adapter;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import com.joshuahalvorson.shoppingcart.R;
+import com.joshuahalvorson.shoppingcart.model.Product;
+import com.joshuahalvorson.shoppingcart.network.ShoppingCartViewModel;
+import java.util.List;
+
+public class EditProductsRecyclerViewAdapter extends
+        RecyclerView.Adapter<EditProductsRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Product> productList;
+
+    private ShoppingCartViewModel viewModel;
+
+    FragmentActivity activity;
+    AppCompatActivity appCompatActivity;
+
+    public EditProductsRecyclerViewAdapter(List<Product> productList,
+                                           FragmentActivity activity,
+                                           AppCompatActivity appCompatActivity) {
+        this.productList = productList;
+        this.activity = activity;
+        this.appCompatActivity = appCompatActivity;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.product_list_element_layout, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, final int i) {
+        final Product product = productList.get(i);
+
+        viewModel = ViewModelProviders.of(activity).get(ShoppingCartViewModel.class);
+
+        viewHolder.productName.setText(product.getProductName());
+        viewHolder.productDesc.setText(product.getProductDescription());
+        viewHolder.productCost.setText("$" + Double.toString(product.getProductCost()));
+        viewHolder.productOnHand.setText(Integer.toString(product.getProductOnHand()) + " unit(s) left");
+
+        viewHolder.quantitySpinner.setVisibility(View.GONE);
+
+        viewHolder.editButton.setBackground(activity.getResources()
+                .getDrawable(R.drawable.ic_edit_black_24dp));
+        viewHolder.editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //launch edit fragment
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final View view;
+        private TextView productName, productDesc, productCost, productOnHand;
+        private Spinner quantitySpinner;
+        private Button editButton;
+
+        public ViewHolder(View view) {
+            super(view);
+            this.view = view;
+            productName = view.findViewById(R.id.product_name);
+            productCost = view.findViewById(R.id.product_cost);
+            productDesc = view.findViewById(R.id.product_desc);
+            productOnHand = view.findViewById(R.id.product_on_hand);
+            quantitySpinner = view.findViewById(R.id.number_to_add_spinner);
+            editButton = view.findViewById(R.id.action_button);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString();
+        }
+    }
+}
--- app/src/main/java/com/joshuahalvorson/shoppingcart/view/fragment/EditProductsFragment.java
+++ app/src/main/java/com/joshuahalvorson/shoppingcart/view/fragment/EditProductsFragment.java
@@ -1,20 +1,36 @@
 package com.joshuahalvorson.shoppingcart.view.fragment;
 
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.joshuahalvorson.shoppingcart.R;
+import com.joshuahalvorson.shoppingcart.adapter.EditProductsRecyclerViewAdapter;
+import com.joshuahalvorson.shoppingcart.model.Product;
+import com.joshuahalvorson.shoppingcart.network.ShoppingCartViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EditProductsFragment extends Fragment {
+    private ShoppingCartViewModel viewModel;
+
+    private List<Product> products;
+
+    private EditProductsRecyclerViewAdapter adapter;
 
     public EditProductsFragment() {
-
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,4 +43,53 @@ public class EditProductsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_edit_products, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        products = new ArrayList<>();
+        RecyclerView recyclerView = view.findViewById(R.id.edit_products_list);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext()) {
+            @Override
+            public boolean canScrollVertically() {
+                return true;
+            }
+        };
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        DividerItemDecoration dividerItemDecoration =
+                new DividerItemDecoration(
+                        recyclerView.getContext(), layoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        adapter = new EditProductsRecyclerViewAdapter(
+                products,
+                getActivity(),
+                (AppCompatActivity)getActivity());
+
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        viewModel = ViewModelProviders.of(this).get(ShoppingCartViewModel.class);
+
+        getAllProducts();
+    }
+
+    private List<Product> getAllProducts() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                products.addAll(viewModel.getAllProducts());
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        }).start();
+        return null;
+    }
 }
