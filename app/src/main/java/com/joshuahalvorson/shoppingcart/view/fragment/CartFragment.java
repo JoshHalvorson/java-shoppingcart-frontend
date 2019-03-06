395432b3c7e4f02bd92f089d3ec208ac597982be
Josh Halvorson <joshhalvorson16@gmail.com>
2019-03-06 09:30:17 -0800

    Add cart fragment

---
+++ app/src/main/java/com/joshuahalvorson/shoppingcart/view/fragment/CartFragment.java
@@ -0,0 +1,50 @@
+package com.joshuahalvorson.shoppingcart.view.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.joshuahalvorson.shoppingcart.R;
+
+public class CartFragment extends Fragment {
+
+    public CartFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_cart, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
